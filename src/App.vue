98cc042133068869ae8042b3379b088a4dd7ae2a
artisan7 <junrick.bation@gmail.com>
2021-07-03 10:15:39 +0800

    Added Registration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Registration />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Registration from './components/Registration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Registration
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Registration.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>Registration
+    <h2>Issue #</h2>
+    <h1 v-html='latestQueueNo'></h1>
+    <button @click='issueQueueNo'>ISSUE QUEUE #</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Registration',
+  methods: {
+    issueQueueNo() {
+      // TODO: limit queue no to certain maximum
+      this.latestQueueNo++;
+
+      // create new queue item
+      var queue_item = {
+        number: this.latestQueueNo,
+        stage: this.stage,
+      };
+
+      // add queue_item to database
+      console.log(queue_item.number);
+    }
+  },
+  data() {
+    return {
+      latestQueueNo: 0,
+      stage: 0
+    }
+  }
+}
+</script>
