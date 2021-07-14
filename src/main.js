7a71d92e9942fce16bada99fc6a9f28753034e2f
MTHLily <tree.of.pyres@gmail.com>
2021-07-15 04:52:04 +0800

    Admin  scaffolding

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "mdb-vue-ui-kit/css/mdb.min.css";
+import "./assets/app.scss"
 
 createApp(App)
   .use(router)
