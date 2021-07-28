8b6fa2520bd9eaadfa0deb01bc4571587d0c35c8
Dwight Ian De Jesus <ddejesus0320@gmail.com>
2021-07-28 15:35:08 +0800

    Test

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="container">
-    <div class="col">
-      <div class="d-flex">
-        <button
-          class="btn btn-danger"
-          @click="localResetQueue"
-          :disabled="processing"
-        >
-          Reset Queue
-        </button>
-        <button class="btn" @click="localSeedUsers" :disabled="processing">
-          Seed Users
-        </button>
-        <button
-          class="btn btn-primary"
-          @click="testQueries"
-          :disabled="processing"
-        >
-          Run Test Queries
-        </button>
-      </div>
-    </div>
+    <div class="container">
+        <div class="col">
+            <div class="d-flex">
+                <button
+                    class="btn btn-danger"
+                    @click="localResetQueue"
+                    :disabled="processing"
+                >
+                    Reset Queue
+                </button>
+                <button
+                    class="btn"
+                    @click="localSeedUsers"
+                    :disabled="processing"
+                >
+                    Seed Users
+                </button>
+                <button
+                    class="btn btn-primary"
+                    @click="testQueries"
+                    :disabled="processing"
+                >
+                    Run Test Queries
+                </button>
+            </div>
+        </div>
 
-    <queue-list-table :queueNumList="queueNumList"></queue-list-table>
-  </div>
+        <queue-list-table :queueNumList="queueNumList"></queue-list-table>
+    </div>
 </template>
 
 <script>
@@ -33,177 +37,184 @@ import { ref, watch } from "@vue/runtime-core";
 import QueueListTable from "../components/QueueListTable.vue";
 
 export default {
-  components: { QueueListTable },
-  setup() {
-    const { seedUsers, resetQueue, runTestQueries, getQueueNums } = useAdmin();
-    const { testQuery, queryStatus } = runTestQueries();
-    const { seedUserFn, seedStatus } = seedUsers();
-    const queueNumList = getQueueNums();
-    const processing = ref(false);
+    components: { QueueListTable },
+    setup() {
+        const {
+            seedUsers,
+            resetQueue,
+            runTestQueries,
+            getQueueNums,
+        } = useAdmin();
+        const { testQuery, queryStatus } = runTestQueries();
+        const { seedUserFn, seedStatus } = seedUsers();
+        const queueNumList = getQueueNums();
+        const processing = ref(false);
 
-    const localSeedUsers = () => {
-      processing.value = true;
+        const localSeedUsers = () => {
+            let sure = prompt("Are you sure?", null);
+            if (sure === null) return;
+            processing.value = true;
 
-      seedUserFn()
-        .then((message) => {
-          createToast(
-            {
-              title: "Success!",
-              description: message,
-            },
-            {
-              type: "sucess",
-              position: "top-center",
+            seedUserFn()
+                .then(message => {
+                    createToast(
+                        {
+                            title: "Success!",
+                            description: message,
+                        },
+                        {
+                            type: "sucess",
+                            position: "top-center",
+                        }
+                    );
+
+                    processing.value = false;
+                })
+                .catch(err => {
+                    createToast(
+                        {
+                            title: "Error",
+                            description: err,
+                        },
+                        {
+                            type: "danger",
+                            position: "top-center",
+                        }
+                    );
+                    processing.value = false;
+                });
+        };
+
+        const localResetQueue = () => {
+            console.log("Resetting...");
+
+            processing.value = true;
+            resetQueue()
+                .then(message => {
+                    createToast(
+                        {
+                            title: "Success",
+                            description: message,
+                        },
+                        {
+                            type: "success",
+                            position: "top-center",
+                        }
+                    );
+                    processing.value = false;
+                })
+                .catch(err => {
+                    createToast(
+                        {
+                            title: "Error",
+                            description: err.message,
+                        },
+                        {
+                            type: "danger",
+                            position: "top-center",
+                        }
+                    );
+
+                    processing.value = false;
+                });
+        };
+
+        const testQueries = () => {
+            processing.value = true;
+            testQuery()
+                .then(message => {
+                    createToast(
+                        {
+                            title: "Success",
+                            description: message,
+                        },
+                        {
+                            type: "success",
+                            position: "top-center",
+                        }
+                    );
+
+                    processing.value = false;
+                })
+                .catch(err => {
+                    createToast(
+                        {
+                            title: "Error",
+                            description: err.message,
+                        },
+                        {
+                            type: "danger",
+                            position: "top-center",
+                        }
+                    );
+
+                    processing.value = false;
+                });
+        };
+
+        watch(
+            () => queryStatus.value,
+            newVal => {
+                createToast(
+                    {
+                        title: "Success",
+                        description: newVal.message,
+                    },
+                    {
+                        type: "warning",
+                        position: "bottom-right",
+                    }
+                );
             }
-          );
-
-          processing.value = false;
-        })
-        .catch((err) => {
-          createToast(
-            {
-              title: "Error",
-              description: err,
-            },
-            {
-              type: "danger",
-              position: "top-center",
-            }
-          );
-          processing.value = false;
-        });
-    };
-
-    const localResetQueue = () => {
-      console.log("Resetting...");
-
-      processing.value = true;
-      resetQueue()
-        .then((message) => {
-          createToast(
-            {
-              title: "Success",
-              description: message,
-            },
-            {
-              type: "success",
-              position: "top-center",
-            }
-          );
-          processing.value = false;
-        })
-        .catch((err) => {
-          createToast(
-            {
-              title: "Error",
-              description: err.message,
-            },
-            {
-              type: "danger",
-              position: "top-center",
-            }
-          );
-
-          processing.value = false;
-        });
-    };
-
-    const testQueries = () => {
-      processing.value = true;
-      testQuery()
-        .then((message) => {
-          createToast(
-            {
-              title: "Success",
-              description: message,
-            },
-            {
-              type: "success",
-              position: "top-center",
-            }
-          );
-
-          processing.value = false;
-        })
-        .catch((err) => {
-          createToast(
-            {
-              title: "Error",
-              description: err.message,
-            },
-            {
-              type: "danger",
-              position: "top-center",
-            }
-          );
-
-          processing.value = false;
-        });
-    };
-
-    watch(
-      () => queryStatus.value,
-      (newVal) => {
-        createToast(
-          {
-            title: "Success",
-            description: newVal.message,
-          },
-          {
-            type: "warning",
-            position: "bottom-right",
-          }
         );
-      }
-    );
 
-    watch(
-      () => seedStatus.value,
-      (newVal) => {
-        createToast(
-          {
-            title: "Success",
-            description: newVal.message,
-          },
-          {
-            type: newVal.status,
-            position: "bottom-right",
-            timeout: 3000,
-          }
+        watch(
+            () => seedStatus.value,
+            newVal => {
+                createToast(
+                    {
+                        title: "Success",
+                        description: newVal.message,
+                    },
+                    {
+                        type: newVal.status,
+                        position: "bottom-right",
+                        timeout: 3000,
+                    }
+                );
+            }
         );
-      }
-    );
 
-    const getStage = (stage) => {
-      const actualStage = stage + 1;
-      const stages = [
-        "Rejected",
-        "Issued Num",
-        // "Registration",
-        "Registration",
-        "Screening",
-        "Screening",
-        "Vitals",
-        "Vitals",
-        "Vaccination",
-        "Vaccination",
-        "Post-Vaccination",
-        "Post-Vaccination",
-        "Done",
-      ];
-      return stages[actualStage];
-    };
+        const getStage = stage => {
+            const actualStage = stage + 1;
+            const stages = [
+                "Rejected",
+                "Issued Num",
+                // "Registration",
+                "Registration",
+                "Screening",
+                "Screening",
+                "Vitals",
+                "Vitals",
+                "Vaccination",
+                "Vaccination",
+                "Post-Vaccination",
+                "Post-Vaccination",
+                "Done",
+            ];
+            return stages[actualStage];
+        };
 
-    return {
-      queueNumList,
-      localSeedUsers,
-      localResetQueue,
-      testQueries,
-      queryStatus,
-      processing,
-      getStage,
-    };
-  },
+        return {
+            queueNumList,
+            localSeedUsers,
+            localResetQueue,
+            testQueries,
+            queryStatus,
+            processing,
+            getStage,
+        };
+    },
 };
 </script>
 
